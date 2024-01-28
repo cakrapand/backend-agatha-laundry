2c1d88833efb80a723f75dd6d7199cff72074a1c
cakrapand <cakrapand@gmail.com>
2024-01-29 02:31:07 +0700

    chore: update build config

--- src/helpers/token.ts
+++ src/helpers/token.ts
@@ -5,7 +5,8 @@ const secretToken = process.env.ACCESS_TOKEN_SECRET;
 
 export const generateToken = (data: ITokenPayload) => {
   if (!secretToken) throw Error("Secret token not set");
-  return sign(data, secretToken, { expiresIn: "1d" });
+  return sign(data, secretToken);
+  // return sign(data, secretToken, { expiresIn: "1d" });
 };
 
 export const verifyToken = (token: string) => {
